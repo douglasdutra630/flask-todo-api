d2f6cbbd7cf936f6d63b4059438eea4017c14961
douglasdutra630 <douglas.dutra630@gmail.com>
2020-04-06 10:38:05 -0600

    adding classes

--- app.py
+++ app.py
@@ -8,8 +8,31 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
 
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class ToDo(db.Model):
+    __tablename__ = "todos"
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100) nullable=False)
+    done = db.Column(db.Boolean)
+
+    def __init__(self, title, done):
+        self.title = title
+        self.done = done
+
+class TodoSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'done')
+
+todo_schema = TodoSchema()
+todos_schema = TodoSchema(many=True)
+
+@app.route("/", methods=["GET"])
+def home():
+    return "<h1> Half dark, half light... just like me</h1>"
+
 
 if __name__ == "__main__":
     app.debug = True
     app.run()
-    
