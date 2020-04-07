65f425fbd2bd94b8289f89b3b74c4488bf45ea46
douglasdutra630 <douglas.dutra630@gmail.com>
2020-04-07 10:42:43 -0600

    added routes

--- app.py
+++ app.py
@@ -36,12 +36,43 @@ def home():
 @app.route("/todos", methods=["GET"])
 def get_todos():
     all_todos = Todo.query.all()
-    result = todo_schema.dump(all_todos)
+    result = todos_schema.dump(all_todos)
     return jsonify(result)
 
 #POST
+@app.route("/todo", methods=["POST"])
+def add_todo():
+    title = request.json['title']
+    done = request.json['done']
+
+    new_todo = Todo(title, done)
+
+    db.session.add(new_todo)
+    db.session.commit()
+
+    todo = Todo.query.get(new_todo.id)
+    return todo_schema.jsonify(todo)
+
 #PUT / PATCH
+@app.route("/todo/<id>", methods=["PATCH"])
+def update_todo(id):
+    todo = Todo.query.get(id)
+
+    new_done = request.json['done']
+
+    todo.done = new_done
+
+    db.session.commit()
+    return todo_schema.jsonify(todo)
+
 #DELETE
+@app.route('/todo/<id>')
+def delete_todo(id):
+    record = Todo.query.get(id)
+    db.session.delete(record)
+    db.session.commit()
+
+    return jsonify('Deleted.')
 
 if __name__ == "__main__":
     app.debug = True
