b878b0c784d005685e4fa7124376182e54de9c7b
douglasdutra630 <douglas.dutra630@gmail.com>
2020-04-08 09:38:09 -0600

    fixing bugs

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 
 app = Flask(__name__)
 CORS(app)
-heroku = heroku(app)
+heroku = Heroku(app)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
